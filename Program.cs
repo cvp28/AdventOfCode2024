8186b60c2f2d5cb51d74f4c1b77a2702f86442fb
cvp28 <carson.vp28@gmail.com>
2024-12-03 09:15:58 -0800

    Day 3 - Release

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Day3.Solution());
-
         if (args.Length == 0) return;
         if (!int.TryParse(args[0], out int Day)) return;
 
@@ -12,6 +10,7 @@ public class Program
         {
             1 => Day1.Solution(),
             2 => Day2.Solution(),
+            3 => Day3.Solution(),
 
             _ => string.Empty
         });
