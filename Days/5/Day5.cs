47ee876ded0a1812ae4a033a64bbbfd9e1f239f3
Carson Ver Planck <carson.vp28@gmail.com>
2024-12-04 22:53:27 -0800

    Day 5
    IComparer and Sort() are the giants that carried me in this one.
    Without it, it would be so much harder for me to solve this in any reasonable amount of time.

---
+++ Days/5/Day5.cs
@@ -0,0 +1,84 @@
+﻿
+internal class Day5
+{
+	static Dictionary<int, List<int>> Rules = [];
+	static List<List<int>> Updates = [];
+
+	public static (int Pt1Count, int Pt2Count) Solution()
+	{
+		var Input = File.ReadAllLines(@".\Days\5\input.txt");
+		var Pt1Count = 0;
+		var Pt2Count = 0;
+
+		// Construct list of rules
+		var rs = Input.Where(l => l.Contains("|"));
+		foreach (var r in rs)
+		{
+			var elems = r.Split('|');
+			var key = int.Parse(elems[0]);
+			var val = int.Parse(elems[1]);
+
+			if (!Rules.ContainsKey(key))
+				Rules[key] = [];
+
+			Rules[key].Add(val);
+        }
+
+		// Construct list of updates
+		var us = Input.Where(l => l.Contains(","));
+		foreach (var u in us)
+		{
+			List<int> temp = [];
+
+			var elems = u.Split(',');
+
+			foreach (var elem in elems)
+				temp.Add(int.Parse(elem));
+
+			Updates.Add(temp);
+		}
+
+		foreach (var Update in Updates)
+		{
+			var MiddleIndex = Update.Count / 2;
+
+			if (TestUpdate(Update))
+			{
+				Pt1Count += Update[MiddleIndex];
+			}
+			else
+			{
+				Update.Sort((i, j) => !Rules[i].Contains(j) ? 1 : -1);
+				Pt2Count += Update[MiddleIndex];
+			}
+		 }
+
+        return (Pt1Count, Pt2Count);
+	}
+
+	private static bool TestUpdate(List<int> Update)
+	{
+		bool IsValid = true;
+
+		for (int i = 0; i < Update.Count; i++)
+		{
+			if (!Rules.TryGetValue(Update[i], out var Rule))
+				continue;
+
+			var ForwardCheck = Update[(i + 1) .. Update.Count];
+			var BackwardCheck = Update[0 .. i];
+
+			IsValid &= ForwardCheck.All(Value =>
+			{
+				if (!Rules.TryGetValue(Value, out var TestRule))
+					return true;
+
+				return !TestRule.Contains(Update[i]);
+			});
+
+			IsValid &= BackwardCheck.All(Value => !Rule.Contains(Value));
+		}
+
+		return IsValid;
+	}
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ public class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(Day5.Solution());
+
         if (args.Length == 0) return;
         if (!int.TryParse(args[0], out int Day)) return;
 
@@ -12,6 +14,7 @@ public class Program
             2 => Day2.Solution(),
             3 => Day3.Solution(),
             4 => Day4.Solution(),
+            5 => Day5.Solution(),
 
             _ => string.Empty
         });
